533c8c35a8520e7b3ce9acf955463472d0bbe694
童远山 <tongweizj@gmail.com>
2018-02-19 11:39:36 -0600

    完成功能 1. 配置文件支持背诵单词库 2. 前台显示单词

---
+++ 鑫鑫背单词/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  鑫鑫背单词
+//
+//  Created by 童玮 on 2018-01-23.
+//  Copyright © 2018 yoese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
